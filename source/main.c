35d778ab915ae56e3ecb2d8627f9d7fe098cbf2b
Manuel Limones <limonesu.me@gmail.com>
2015-04-16 06:16:33 -0500

    add button sense configuration

--- source/main.c
+++ source/main.c
@@ -1,12 +1,17 @@
 #include "stm32f4xx.h"
 
 void led_init(void);
+void button_init(void);
 
 int main(){
 	led_init();
-	GPIO_SetBits(GPIOA,GPIO_Pin_5);
+	button_init();
 	while(1){
-		
+		if(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_13)){
+			GPIO_ResetBits(GPIOA,GPIO_Pin_5);
+		} else{
+			GPIO_SetBits(GPIOA,GPIO_Pin_5);
+		}
 	}
 }
 
@@ -17,4 +22,13 @@ void led_init(void){
 	myGPIO.GPIO_Mode=GPIO_Mode_OUT;
 	myGPIO.GPIO_Pin=GPIO_Pin_5;
 	GPIO_Init(GPIOA,&myGPIO);
+}
+
+void button_init(void){
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC,ENABLE);
+	GPIO_InitTypeDef myGPIO;
+	GPIO_StructInit(&myGPIO);
+	myGPIO.GPIO_Mode=GPIO_Mode_IN;
+	myGPIO.GPIO_Pin=GPIO_Pin_13;
+	GPIO_Init(GPIOC,&myGPIO);
 }
