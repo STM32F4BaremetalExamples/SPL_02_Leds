8940fe0ae6e187280f8d35a74b5be93ec533b3f6
Manuel Limones <limonesu.me@gmail.com>
2015-04-16 05:22:42 -0500

    fix bug: wrong pin and port assigned no led

--- source/main.c
+++ source/main.c
@@ -4,16 +4,17 @@ void led_init(void);
 
 int main(){
 	led_init();
-	GPIO_SetBits(GPIOC,GPIO_Pin_13);
+	GPIO_SetBits(GPIOA,GPIO_Pin_5);
 	while(1){
 		
 	}
 }
 
 void led_init(void){
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
 	GPIO_InitTypeDef myGPIO;
 	GPIO_StructInit(&myGPIO);
 	myGPIO.GPIO_Mode=GPIO_Mode_OUT;
-	myGPIO.GPIO_Pin=GPIO_Pin_13;
-	GPIO_Init(GPIOC,&myGPIO);
+	myGPIO.GPIO_Pin=GPIO_Pin_5;
+	GPIO_Init(GPIOA,&myGPIO);
 }
